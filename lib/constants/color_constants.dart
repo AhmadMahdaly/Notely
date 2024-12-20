3653c0c78f7e58bda74fb3e382ebd67570fc6dd7
Ahmed Mahdaly <ahmed.mahdaly86@gmail.com>
2024-12-20 22:56:25 +0200

    Add Color constants

---
+++ lib/constants/color_constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kDarkColor = Color(0xff0E0F19);
+const klLessDarkColor = Color(0xff466060);
+const kMainColor = Color(0xff57886C);
+const kMainTextColor = Color(0xff81A684);
+const kShadowColor = Color(0xffF8C7CC);
