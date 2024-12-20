d8063417e9c94baa9584b3532371d9b784c38814
Ahmed Mahdaly <ahmed.mahdaly86@gmail.com>
2024-12-21 00:49:43 +0200

    Create function one time onboard

---
+++ lib/widgets/one_time_onboard_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:notely/screens/home_page.dart';
+import 'package:notely/screens/onboard_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class OneTimeOnboard extends StatefulWidget {
+  const OneTimeOnboard({super.key});
+
+  @override
+  State<OneTimeOnboard> createState() => _OneTimeOnboardState();
+}
+
+class _OneTimeOnboardState extends State<OneTimeOnboard> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _checkOnboarding(),
+      builder: (context, snapshot) {
+        if (snapshot.data != true) {
+          return const OnboardPage();
+        } else {
+          return const HomePage();
+        }
+      },
+    );
+  }
+
+  Future<bool> _checkOnboarding() async {
+    final prefs = await SharedPreferences.getInstance();
+    final hasSeenOnboarding = prefs.getBool('hasSeenOnboarding') ?? false;
+    if (!hasSeenOnboarding) {
+      await prefs.setBool('hasSeenOnboarding', true);
+    }
+    return hasSeenOnboarding;
+  }
+}
